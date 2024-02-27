c8d5d1a867df0b3cb4d0fb3d8831e99601bc6fe9
Francesco Novy <francesco.novy@sentry.io>
2024-02-27 12:26:59 +0000

    ref(browser): Store browser metrics as attributes instead of tags

--- packages/tracing-internal/src/browser/metrics/index.ts
+++ packages/tracing-internal/src/browser/metrics/index.ts
@@ -448,15 +448,11 @@ function _trackNavigator(transaction: Transaction): void {
   const connection = navigator.connection;
   if (connection) {
     if (connection.effectiveType) {
-      // TODO: Can we rewrite this to an attribute?
-      // eslint-disable-next-line deprecation/deprecation
-      transaction.setTag('effectiveConnectionType', connection.effectiveType);
+      transaction.setAttribute('effectiveConnectionType', connection.effectiveType);
     }
 
     if (connection.type) {
-      // TODO: Can we rewrite this to an attribute?
-      // eslint-disable-next-line deprecation/deprecation
-      transaction.setTag('connectionType', connection.type);
+      transaction.setAttribute('connectionType', connection.type);
     }
 
     if (isMeasurementValue(connection.rtt)) {
@@ -465,15 +461,11 @@ function _trackNavigator(transaction: Transaction): void {
   }
 
   if (isMeasurementValue(navigator.deviceMemory)) {
-    // TODO: Can we rewrite this to an attribute?
-    // eslint-disable-next-line deprecation/deprecation
-    transaction.setTag('deviceMemory', `${navigator.deviceMemory} GB`);
+    transaction.setAttribute('deviceMemory', `${navigator.deviceMemory} GB`);
   }
 
   if (isMeasurementValue(navigator.hardwareConcurrency)) {
-    // TODO: Can we rewrite this to an attribute?
-    // eslint-disable-next-line deprecation/deprecation
-    transaction.setTag('hardwareConcurrency', String(navigator.hardwareConcurrency));
+    transaction.setAttribute('hardwareConcurrency', String(navigator.hardwareConcurrency));
   }
 }
 
@@ -485,36 +477,26 @@ function _tagMetricInfo(transaction: Transaction): void {
     // Capture Properties of the LCP element that contributes to the LCP.
 
     if (_lcpEntry.element) {
-      // TODO: Can we rewrite this to an attribute?
-      // eslint-disable-next-line deprecation/deprecation
-      transaction.setTag('lcp.element', htmlTreeAsString(_lcpEntry.element));
+      transaction.setAttribute('lcp.element', htmlTreeAsString(_lcpEntry.element));
     }
 
     if (_lcpEntry.id) {
-      // TODO: Can we rewrite this to an attribute?
-      // eslint-disable-next-line deprecation/deprecation
-      transaction.setTag('lcp.id', _lcpEntry.id);
+      transaction.setAttribute('lcp.id', _lcpEntry.id);
     }
 
     if (_lcpEntry.url) {
       // Trim URL to the first 200 characters.
-      // TODO: Can we rewrite this to an attribute?
-      // eslint-disable-next-line deprecation/deprecation
-      transaction.setTag('lcp.url', _lcpEntry.url.trim().slice(0, 200));
+      transaction.setAttribute('lcp.url', _lcpEntry.url.trim().slice(0, 200));
     }
 
-    // TODO: Can we rewrite this to an attribute?
-    // eslint-disable-next-line deprecation/deprecation
-    transaction.setTag('lcp.size', _lcpEntry.size);
+    transaction.setAttribute('lcp.size', _lcpEntry.size);
   }
 
   // See: https://developer.mozilla.org/en-US/docs/Web/API/LayoutShift
   if (_clsEntry && _clsEntry.sources) {
     DEBUG_BUILD && logger.log('[Measurements] Adding CLS Data');
     _clsEntry.sources.forEach((source, index) =>
-      // TODO: Can we rewrite this to an attribute?
-      // eslint-disable-next-line deprecation/deprecation
-      transaction.setTag(`cls.source.${index + 1}`, htmlTreeAsString(source.node)),
+      transaction.setAttribute(`cls.source.${index + 1}`, htmlTreeAsString(source.node)),
     );
   }
 }
